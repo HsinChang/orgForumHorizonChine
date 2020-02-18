5a85f2b8014987ef20c114cbe10dc4a9a66c1be7
Hsin Chang <mr.hsin.chang@gmail.com>
2020-02-18 22:27:41 +0100

    MongoDB configured

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const path = require('path');
 const expressEdge = require('express-edge');
 const express = require('express');
+const mongoose = require('mongoose');
  
 const app = new express();
- 
+
+mongoose.connect('mongodb://localhost:27017/node-blog', { useNewUrlParser: true })
+    .then(() => 'You are now connected to Mongo!')
+    .catch(err => console.error('Something went wrong', err));
+
 app.use('/assets', express.static('assets'));
 app.use(expressEdge.engine);
 
