49a0e993a5bd4be56a2f1c9392549ab0edbae111
Hsin Chang <mr.hsin.chang@gmail.com>
2020-02-18 21:24:02 +0100

    Adopted express-edge template engine

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const path = require('path');
- 
+const expressEdge = require('express-edge');
 const express = require('express');
  
 const app = new express();
  
 app.use('/assets', express.static('assets'));
+app.use(expressEdge.engine);
+
+app.set('views', __dirname + '/views');
  
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'index.html'));
@@ -16,6 +19,10 @@ app.get('/index.html', (req, res) => {
 
 app.get('/about.html', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'about.html'));
+});
+
+app.get('/offers', (req, res) => {
+    res.render('offers');
 });
  
 app.listen(4000, () => {
